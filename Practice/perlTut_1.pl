e60acf31de95f97a541d986025b741f74dda509a
OfficialShubhamRane <official.shubhamrane@gmail.com>
2021-12-06 13:19:53 -0600

    basics

--- Practice/perlTut_1.pl
+++ Practice/perlTut_1.pl
@@ -5,3 +5,19 @@
 # use feature "switch";
 
 print "Hello from perlTut_1\n";
+
+my $name = "Shubham";
+my $age = 24;
+my $addr = "123 Main St";
+
+my $my_info = "My name is $name, I live on \"$addr\". ";
+
+$my_info = qq{$my_info \n};
+
+print $my_info;
+
+my $first = 1;
+my $second = 2;
+
+($first, $second) = ($second, $first);
+print qq{first: $first, second: $second \n};
